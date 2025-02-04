280aaed5379820b5240b3730ef7b1325a24cd5fb
Pqeuaindatento <dolinho899@gmail.com>
2025-02-04 09:48:55 -0300

    eu quero me matar

---
+++ teste 1.py
@@ -0,0 +1,134 @@
+import os
+
+
+print("\n\nVocê é um aventureiro, e em sua primeira missão é derrotar um goblin\n\n")
+input()
+
+print("Se conseguir, ganhará uma recompensa.\n\n")
+input()
+
+print("Você possui 100 PV e 20 MP\n\n")
+input()
+os.system("cls")
+
+print("100/100 PV \n\n 20/20 MP====\n\n")
+
+
+
+A = 100-5
+B = 95-5
+B2 = 99-5
+C = 90-1
+C2 = 89-5
+D = 85-5
+D2 = 84-5
+E = 20-5
+F = 15-5
+G = 10-5
+H = 5-5
+I = 40
+I2 = 40
+J = 30
+K = 20
+L = 10
+
+list['a'=='-10', 'b' =='defense']
+a='a'
+b='b'
+
+list1 = str(input("\nEscolha uma ação: Escolha uma ação: \n\n(a) ataque = -10 \n\n (b) defesa\n\n"))
+            
+os.system("cls")
+    
+if list == a:
+    
+         print("Voce causou um dano, mas também levou dano\n\n")
+         input()
+         
+         print(A,"/100 PV \n\n ",E,"/20 MP===\n\n")
+         
+         print(I, "de vida do Globin")
+         
+else:
+  
+         print("você não atacou, mas voce levou 1 de dano\n\n 99 PV \n\n 15/20 MP===")
+         
+         print(I2, "de vida do Globin") 
+         
+
+input()
+
+
+
+
+    
+    
+list2 = str(input("\nEscolha uma ação: \n\n (a) ataque = -10 \n\n (b) defesa\n\n"))
+            
+os.system("cls")
+    
+if list == a:
+         print("Voce causou um dano, mas também levou dano\n\n")
+         input()
+         
+         
+         print(B or B2,"/100 PV \n\n ",F,"/20 MP==\n\n")
+         
+         print(J ,"de vida do Globin")
+         
+         
+         
+else:
+    
+    
+     print("você não atacou, mas voce levou 1 de dano\n\n 98 PV \n\n 10/20 MP===")
+print(I2, "de vida do Globin")
+     
+    
+    
+list3 = str(input("\nEscolha uma ação: \n\n (a) ataque = -10 \n\n (b) defesa\n\n"))
+            
+os.system("cls")
+    
+if list == a:
+         print("Voce causou um dano, mas também levou dano\n\n")
+         
+         print(C or C2,"/100 PV \n\n ",G,"/20 MP=\n\n")
+         print(K ,"de vida do Globin")
+         
+else:
+    print("você não atacou, mas voce levou 1 de dano\n\n 97 PV \n\n 5/20 MP===")
+    print(I2, "de vida do Globin")
+
+
+
+    
+    
+list4 = str(input("\nEscolha uma ação: \n\n (a) ataque = -10 \n\n (b) defesa\n\n"))
+            
+os.system("cls")
+    
+if list == a:
+         print("Voce causou um dano, mas também levou dano\n\n")
+         print(D or D2,"/100 PV \n\n ",H,"/20 MP\n\n")
+         print(L ,"de vida do Globin")
+         print("Parabens, sua primeira vitória!!!\n\nSua recompensa é...\n\n")
+         input()
+         print("lâmina globin --> ataque = 15  evasão = 3 \n\n Bônus --> Veneno de cobra com 2 de DPS \n\n        ---Fim da Demo---        ")
+         
+else:
+    
+    
+    
+         print("você não atacou, mas voce levou 1 de dano\n\n 96 PV \n\n 0/20 MP=== \n\nVocê não aguentou e ficou cansado e paralisado, o globin aproveita e ataca e então, você morreu de vários ataques\n\n        ---Fim de jogo---     ") 
+
+   
+       
+
+    
+   
+
+
+
+
+
